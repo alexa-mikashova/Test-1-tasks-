c050e5ee9f361011e33cc80106d39e9113b9ad78
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-11-13 21:56:55 +0300

    Add solution to Task 2

---
+++ Task2/Task1.2/Task1.2/IntExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1_2
+{
+    public static class IntExtensions
+    {
+        private static string[] StringNumbers = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+
+        private static string[] stringNumersArray;
+        private static string numberString;
+
+        public static string ToString(int number)
+        {
+            numberString = number.ToString();
+
+            stringNumersArray = new string[numberString.Length];
+
+            for (int i = 0; i < stringNumersArray.Length; i++)
+            {
+                stringNumersArray[i] = StringNumbers[int.Parse(numberString[i].ToString())];
+            }
+
+            return string.Join(" ", stringNumersArray.ToArray());
+        }
+    }
+}
---
+++ Task2/Task1.2/Task1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Task1_2;
+int number;
+bool isParceSuccessOne;
+do
+{
+    Console.Write("Ведите целочисленное число: ");
+    isParceSuccessOne = int.TryParse(Console.ReadLine(), out number);
+}
+while (!isParceSuccessOne);
+
+Console.WriteLine($"{IntExtensions.ToString(number)}");
