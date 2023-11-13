680cae06fbdd97ecf080ab32bfb411a931c07d43
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-11-13 21:45:43 +0300

    Add solution to Task 1

---
+++ Task1/Task1.1/Task1.1/MyStack.cs
@@ -0,0 +1,45 @@
+﻿namespace Task1._1
+{
+    internal class MyStack
+    {
+        private int[] stack;
+        private int top;
+
+        public MyStack(int size)
+        {
+            stack = new int[size];
+            top = -1;
+        }
+
+        public void Push(int value)
+        {
+            if (top >= stack.Length - 1)
+            {
+                Console.WriteLine("Массив stack переполнен!");
+            }
+            else
+            {
+                ++top;
+                stack[top] = value;
+                Console.WriteLine($"Число {value} добавлено в stack");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Массив stack пуст!");
+                return -1;
+            }
+            else
+            {
+                int value = stack[top];
+                Console.WriteLine($"Число {value} удалено из stack");
+                stack[top] = 0;
+                --top;
+                return value;
+            }
+        }
+    }
+}
---
+++ Task1/Task1.1/Task1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Task1._1;
+int size;
+bool isParceSuccessOne;
+do
+{
+    Console.Write("Введите размер stack: ");
+    isParceSuccessOne = int.TryParse(Console.ReadLine(), out size);
+}
+while (!isParceSuccessOne || size == 0);
+
+MyStack myStack = new MyStack(size);
+myStack.Push(2);
+myStack.Push(5);
+myStack.Push(6);
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
